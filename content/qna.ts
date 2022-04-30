372e61b8003d80c5228224264d128be1d9a78592
AkashRajpurohit <akashrajpurohit.009@gmail.com>
2022-04-30 13:50:35 +0530

    chore: :card_file_box: initial qna data added

---
+++ content/qna.ts
@@ -0,0 +1,137 @@
+const qna = [
+  {
+    question: `You are overcomplicating this`,
+    answer: `Being mindful of timelines. Let’s concentrate on the initial scope.`,
+  },
+  {
+    question: `That meeting sounds like a waste of my time`,
+    answer: `I’m unable to add value to this meeting but I would be happy to review the minutes.`,
+  },
+  {
+    question: `I told you so`,
+    answer: `As per my prediction, this outcome does not come as a surprise.`,
+  },
+  {
+    question: `That sounds like a horrible idea`,
+    answer: `Are we confident that this is the best solution or are we still exploring alternatives?`,
+  },
+  {
+    question: `I already told you this`,
+    answer: `As Indicated prior`,
+  },
+  {
+    question: `Can you answer all of the questions I asked and not just pick and choose one`,
+    answer: `Are you able to provide some clarity around the other questions previously asked?`,
+  },
+  {
+    question: `Did you even read my email?`,
+    answer: `Reattaching my email to provide further clarity`,
+    alternativeAnswers: [`I’ll provide an update when I have one`],
+  },
+  {
+    question: `Stop bothering me`,
+    answer: `You have not heard from the because further information is not available at this time, Once I have an update I’ll be sure to loop you in.`,
+  },
+  {
+    question: `I don’t want to talk to you right now!`,
+    answer: `I a currently tied up with something but I will connect with you once I am free.`,
+  },
+  {
+    question: `Do your job!`,
+    answer: `It is my understanding that you are the appropriate person to contact in regards to this. But If there’s is someone better equipped for this let me know.`,
+  },
+  {
+    question: `That's not my job`,
+    answer: `This falls outside of my responsibilities but I would be happy to connect you with someone who can help.`,
+  },
+  {
+    question: `Stop assigning me so many tasks if you want any of them to get done`,
+    answer: `As my workload is quite heavy, can you help me understand what I should reprioritize to accommodate this new task?`,
+  },
+  {
+    question: `answer my emails`,
+    answer: `If There’s a better way to get in contact with you please let me know as I am hoping to have this resolved as soon as possible`,
+  },
+  {
+    question: `This is not my problem`,
+    answer: `I recommend directing this issue to [Name] as they have the proper expertise to best assist you`,
+  },
+  {
+    question: `If you would have read the whole email you’d know the answer to this`,
+    answer: `I have included my initial email below which contains all of the details you are looking for.`,
+  },
+  {
+    question: `I have absolutely no idea what you are talking about`,
+    answer: `Can you help me better understand what exactly is it that you require on my end?`,
+  },
+  {
+    question: `Stop micromanaging`,
+    answer: `I am confident in my ability to complete this project and will be sure to reach out, If or when I require your input.`,
+  },
+  {
+    question: `Please hurry and get this done!`,
+    answer: `It is important that we have this completed in order to meet our targeted deadlines which are quickly approaching.`,
+  },
+  {
+    question: `Stay in your own lane`,
+    answer: `Thank you for your input. I’ll keep that in mind. As I move forward with decisions that fall within my responsibilities`,
+  },
+  {
+    question: `I’ve told you this multiple times`,
+    answer: `There seems to be a disconnect here as this information has already been provided`,
+  },
+  {
+    question: `I’m not doing your job for you`,
+    answer: `I do not have the capacity to take this on in addition to my own workload but I’m happy to support where it makes sense.`,
+  },
+  {
+    question: `We do not need to have a meeting about this.`,
+    answer: `Being respectful of everyone's time let's discuss this through email until we have a more defined agenda`,
+  },
+  {
+    question: `Did you just take credit for my work?`,
+    answer: `It is great to see my ideas being exposed to a wider audience and I would have appreciated the opportunity to have been included in the delivery.`,
+  },
+  {
+    question: `Google that your self`,
+    answer: `The internet is a great resource for these types of questions and I am available to clarify elements that you are not able to find online.`,
+  },
+  {
+    question: `What you are saying does not make sense`,
+    answer: `We seem to have a different understanding on this. Can you elaborate further on your thought process here?`,
+  },
+  {
+    question: `I am not paid enough to do this`,
+    answer: `This falls out of my job description but if the opportunity for a role expansion becomes available I would be happy to discuss reworking my contract to better align with these new responsibilities`,
+  },
+  {
+    question: `I totally forgot about your email`,
+    answer: `Thank you for your patience`,
+  },
+  {
+    question: `I'm going to need a whole lot of more information if you want this me to do this`,
+    answer: `Please let me know when further details become available as I require more information to successfully complete this task`,
+  },
+  {
+    question: `Stop calling me before my workday even starts`,
+    answer: `If you need to contact me, please note that my working hours being at 8 am and 6 pm communications received prior to this won't be seen.`,
+  },
+  {
+    question: `Check your inbox, I already sent this to you!`,
+    answer: `I previously sent you an email regarding that but please let me know if something went wrong in transit`,
+  },
+  {
+    question: `I couldn't care less`,
+    answer: `I will defer to your judgment on this as I am not passionate either way and I trust your expertise.`,
+  },
+  {
+    question: `I told you so and now this is your problem`,
+    answer: `I did previosly note that this was a likely outcome. How do you plan to resolve this?`,
+  },
+  {
+    question: `Stop trying to make me do your work!`,
+    answer: `I am not able to offer you additional support in completing your workload`,
+  },
+];
+
+export default qna;
