2816622f52001f9328c1b10915ed5c80a3ce6b3e
AkashRajpurohit <akashrajpurohit.009@gmail.com>
2022-04-30 19:22:35 +0530

    perf: :zap: use preact for client prod builds

--- next.config.js
+++ next.config.js
@@ -12,4 +12,16 @@ module.exports = {
       ],
     },
   ],
+  webpack: (config, { dev, isServer }) => {
+    if (!dev && !isServer) {
+      // Replace React with Preact only in client production build
+      Object.assign(config.resolve.alias, {
+        'react/jsx-runtime.js': 'preact/compat/jsx-runtime',
+        react: 'preact/compat',
+        'react-dom/test-utils': 'preact/test-utils',
+        'react-dom': 'preact/compat',
+      });
+    }
+    return config;
+  },
 };
