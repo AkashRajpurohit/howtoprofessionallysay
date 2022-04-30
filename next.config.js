340b8f886025f02473a1087bd4b8781e65c38924
AkashRajpurohit <akashrajpurohit.009@gmail.com>
2022-04-30 19:20:31 +0530

    perf: :zap: cache fonts on cdn

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-}
+  headers: [
+    {
+      source: '/fonts/:path*',
+      headers: [
+        {
+          key: 'Cache-Control',
+          value: 'public, max-age=31536000, immutable',
+        },
+      ],
+    },
+  ],
+};
