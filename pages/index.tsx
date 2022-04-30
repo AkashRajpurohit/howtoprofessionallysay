b93014cd9364414db3ba6d4cefa37c0ef0f411e8
AkashRajpurohit <akashrajpurohit.009@gmail.com>
2022-04-30 13:36:31 +0530

    feat: :tada: project init

---
+++ pages/index.tsx
@@ -0,0 +1,20 @@
+import type { NextPage } from 'next';
+import Head from 'next/head';
+import Image from 'next/image';
+
+const Home: NextPage = () => {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+      <Head>
+        <title>How do you professionally say</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
+        Hello World
+      </main>
+    </div>
+  );
+};
+
+export default Home;
