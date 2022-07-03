5eb57dbbf652166fb1f0f081f57f37011caf6308
AkashRajpurohit <akashrajpurohit.009@gmail.com>
2022-07-03 15:04:30 +0530

    feat: :lipstick: cosmetic changes

--- components/LayoutWrapper.tsx
+++ components/LayoutWrapper.tsx
@@ -9,8 +9,8 @@ const LayoutWrapper: FunctionComponent<ILayoutWrapperProps> = ({
   return (
     <div className="mx-auto flex min-h-[50vh] flex-col-reverse justify-evenly px-4 py-8 lg:flex-row xl:gap-6 xl:px-0">
       <aside className="w-0 print:hidden print:h-0 xl:h-screen 2xl:w-80"></aside>
-      <main className="mb-auto w-full lg:max-w-3xl">{children}</main>
-      <aside className="top-0 pt-12 print:hidden print:h-0 lg:sticky lg:h-screen lg:w-80">
+      <main className="my-4 mb-auto w-full lg:max-w-3xl">{children}</main>
+      <aside className="top-0 pt-0 print:hidden print:h-0 lg:sticky lg:h-screen lg:w-80 lg:pt-12">
         <Sidebar />
       </aside>
     </div>
