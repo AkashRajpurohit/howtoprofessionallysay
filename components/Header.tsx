234648f30324c859dcbc9ac0b7576270861c637a
Akash Rajpurohit <akashrajpurohit.009@gmail.com>
2023-02-07 19:08:28 +0530

    chore: :memo: update warning text

--- components/Header.tsx
+++ components/Header.tsx
@@ -13,7 +13,7 @@ const Header: FunctionComponent<IHeaderProps> = (): JSX.Element => {
       <p className="text-center text-xl font-bold text-gray-200">
         {siteMetadata.description}{' '}
         <small className="text-sm text-gray-300">
-          (not really, try at your own risk)
+          (try at your own risk)
         </small>
       </p>
       <div className="flex flex-col gap-6 md:flex-row">
