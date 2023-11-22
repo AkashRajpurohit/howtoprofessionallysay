21066e4fe490b9b66a252d7dfbdeb196872e9450
AkashRajpurohit <me@akashrajpurohit.com>
2023-11-22 16:22:43 +0530

    feat: :sparkles: use gforms link to capture users feedback for adding new data

--- components/NoQuestionFound.tsx
+++ components/NoQuestionFound.tsx
@@ -2,25 +2,27 @@ import React, { FunctionComponent } from 'react';
 
 const NoQuestionFound: FunctionComponent = (): JSX.Element => {
   return (
-    <div className="mt-4 flex flex-col items-center justify-center gap-6">
+    <div className="mt-4 flex flex-col items-center justify-center gap-10">
       <img
         src={'/static/images/not-found.svg'}
         width={250}
         height={300}
         alt="data not found"
       />
-      <p>No data found for this option.</p>
-      <p>
-        Want to add it? Head{' '}
-        <a
-          href="https://github.com/AkashRajpurohit/howtoprofessionallysay/blob/main/CONTRIBUTING.md"
-          target="_blank"
-          rel="noreferrer noopener"
-          className="text-primary-500 hover:text-primary-400"
-        >
-          here
-        </a>
-      </p>
+      <div className="space-y-1 text-center">
+        <p>No data found for this option.</p>
+        <p>
+          Want to add it?{' '}
+          <a
+            href="https://forms.gle/jHygHaDZNEy8YktN8"
+            target="_blank"
+            rel="noreferrer noopener"
+            className="text-primary-500 hover:text-primary-400"
+          >
+            Click here
+          </a>
+        </p>
+      </div>
     </div>
   );
 };
