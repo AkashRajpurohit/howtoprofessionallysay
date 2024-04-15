3ee43a5d4f66649ae6dbe8a28bd0eda70df1cd2f
AkashRajpurohit <me@akashrajpurohit.com>
2024-04-15 19:58:07 +0530

    feat: :sparkles: use env variable for umami

--- components/Analytics.tsx
+++ components/Analytics.tsx
@@ -2,6 +2,7 @@ import { isProduction } from 'lib/utils';
 import Script from 'next/script';
 
 const Umami = () => {
+  const srcUrl = process.env.NEXT_PUBLIC_UMAMI_URL;
   return (
     <>
       <Script
@@ -9,7 +10,7 @@ const Umami = () => {
         defer
         data-cache="true"
         data-website-id="0ef8d12e-17b8-49c9-9c8e-9148683a7123"
-        src="https://rengoku.akashrajpurohit.com/monjiro.js"
+        src={srcUrl}
       />
     </>
   );
